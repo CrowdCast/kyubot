0289e5a00d6bb8d163e9d2d5382a6da9a2359b1a
hkung77 <henrykung@live.ca>
2017-05-01 16:14:41 +0900

    added field validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :requests
+  validates :slack_id, :slack_name, presence: true
+  validates :is_approver, inclusion: { in: [true, false] }
+  validates :allowance, numericality: true
+  validates :days_taken, numericality: true
 end
