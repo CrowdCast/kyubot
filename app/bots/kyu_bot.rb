0b00bd72eac28ac731690a11dd45ca6d089b6f73
hkung77 <henrykung@live.ca>
2017-05-01 17:05:42 +0900

    base for list action

--- app/bots/kyu_bot.rb
+++ app/bots/kyu_bot.rb
@@ -15,4 +15,16 @@ class KyuBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
   end
+
+  command 'list' do |client, data, match|
+    if match['expression']
+      user = User.where(slack_name: match['expression'])
+      client.say(text: user.allowance)
+      client.say(text: 'theres nothing to list', channel: data.channel)
+    else
+      user = User.all()
+      client.say(text: 'listen to my hearttt', channel: data.channel)
+    end
+  end
+
 end
