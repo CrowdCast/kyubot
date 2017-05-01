6670a1701d070315e1b19719dcd31038b691a97b
Haley Koike <haley@crowdcast.jp>
2017-05-01 23:11:29 +0900

    Add potential code for Team and SlackWebService

--- kyubot_code.rb
+++ kyubot_code.rb
@@ -50,3 +50,57 @@ class Bot < SlackRubyBot::Server
     client.owner = @team
   end
 end
+
+# Team class
+class Team < ApplicationRecord
+  has_many :users
+
+  after_create :create_or_update_users
+  after_create :start_bot
+
+  def create_or_update_users
+    users_hash = service_client.get_users
+    users_hash.each { |user_hash|
+      create_or_update_user(user_hash)
+    }
+  end
+
+  def create_or_update_user(hash)
+    user = User.find_by(team: self, slack_id: hash.id)
+    if user
+      user.update({
+        name: hash.slack_name,
+        is_approver: hash.is_admin || hash.is_owner || hash.is_primary_owner,
+        is_deleted: hash.deleted,
+      })
+    else
+      User.create({
+        team: self,
+        name: hash.slack_name,
+        is_approver: hash.is_admin || hash.is_owner || hash.is_primary_owner,
+        is_deleted: hash.deleted,
+        slack_id: hash.id
+      })
+    end
+  end
+
+  def start_bot
+    BotService.instance.start!(self)
+  end
+
+  def service_client
+    @service ||= SlackWebService.new(self.token)
+  end
+end
+
+# Slack web api service
+class SlackWebService
+  def initialize(token)
+    @token = token
+    @client ||= Slack::Client.new(token: @token)
+  end
+
+  def get_users
+    @client.users_list.members
+  end
+end
