36736394dbdc4b02121e40a5a0320b78cb76be3e
Haley Koike <haley@crowdcast.jp>
2017-05-02 14:03:44 +0900

    Add utility methods for Request and User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   belongs_to :team
   has_many :requests
+
+  def days_remaining
+    allowance - days_taken
+  end
+
+  def all_days_requested
+    requests.map{|request| request.days}.flatten
+  end
 end
